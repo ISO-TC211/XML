cfa1d8a27a4874d80cacf351ce780e0b873558bd
tedhabermann <rehabermann@me.com>
2015-04-03 14:38:02 -0600

    Ready to port to standards.iso.org
    Moved all working files to version root directories, removed version
    dates from all files, adjusted relative paths

--- MaintenanceTools/MoveVersionFilesToRoot.sh
+++ MaintenanceTools/MoveVersionFilesToRoot.sh
@@ -1,22 +1,34 @@
 #
 # script for moving schema and associated files out of a dated working directory
-# and into the root version directory, i.e. from xxx/1.0/2014-07-11 to xxx/1.0
+# and into the root version directory, i.e. from xxx/1.0/2014-12-25 to xxx/1.0
 #
-# 2014-08 thabermann@hdfgroup.org 
+# 2015-04 thabermann@hdfgroup.org 
 #
-# the working version directory is 2014-07-11 in this case.
+# the working version directory is 2014-12-25 in this case.
 #
 # run this script from the namespace root/MaintenanceTools
 #
-# copy all files (*) from the working directory to the version root 
-find ../*/*/*/2014-07-11 -name "*" -execdir cp {} .. \;
+# copy all files (*) from the working directory to the version root
+
+# standards with parts (e.g. 19115-2, -3) are at ../standards.iso.org/*/*/*/*/2014-12-25
+echo move files for standards with parts
+find ../standards.iso.org/*/*/*/*/2014-12-25 -name "*" -print
+find ../standards.iso.org/*/*/*/*/2014-12-25 -name "*" -execdir cp {} .. \;
+
+echo move files for standards without parts
+# standards without parts (e.g. 19111) are at ../standards.iso.org/*/*/*/2014-12-25
+find ../standards.iso.org/*/*/*/2014-12-25 -name "*" -print
+find ../standards.iso.org/*/*/*/2014-12-25 -name "*" -execdir cp {} .. \;
 #
 # edit all schema files to remove the working directory and adjust relative paths
 # this will break all of the working directory schemas
-find ../*/*/*/* -name "*.xsd" -exec sed -E -i .bak -f removeVersionDates.sed {} \;
+find ../standards.iso.org -name "*.xsd" -print
+find ../standards.iso.org -name "*.xsd" -exec sed -E -i .bak -f removeVersionDates.sed {} \;
 #
 # delete the backup files created by editng the schema
-find ../ -name "*.bak" -delete
+#find ../standards.iso.org -name "*.bak" -delete
 #
 # remove all of the working directories (2013* and 2014*)
-rm -R -d ../*/*/*/201*
+rm -R -d ../standards.iso.org/*/*/*/201*
+rm -R -d ../standards.iso.org/*/*/*/*/201*
+
--- MaintenanceTools/removeVersionDates.sed
+++ MaintenanceTools/removeVersionDates.sed
@@ -2,7 +2,15 @@
 # These are the regular expressions required for replacing version dates in xsd files
 # thabermann@hdfgroup.org
 #
-s_xmlns:([a-z0-9]{3})="http://www.isotc211.org/namespace/([a-z0-9]*)/([0-9\.]*)/([0-9-]{10})"_xmlns:\1="http://www.isotc211.org/namespace/\1/\3"_g
-s_schemaLocation="../../../../(ISO[0-9-]*)/([a-z0-9]*)/([0-9\.]*)/[0-9-]{10}/[a-z0-9]*.xsd"_schemaLocation="../../../\1/\2/\3/\2.xsd"_g
-s_namespace="http://www.isotc211.org/namespace/([a-z0-9]*)/([0-9\.]*)/[0-9-]{10}"_namespace="http://www.isotc211.org/namespace/\1/\2"_g
-s_targetNamespace="http://www.isotc211.org/namespace/([a-z0-9]*)/([0-9\.]*)/[0-9-]{10}"_targetNamespace="http://www.isotc211.org/namespace/\1/\2"_
+# Originals
+#s_xmlns:([a-z0-9]{3})="http://standards.iso.org/([a-z0-9]*)/([0-9\.]*)/([0-9-]{10})"_xmlns:\1="http://www.isotc211.org/namespace/\1/\3"_g
+#s_schemaLocation="../../../../(ISO[0-9-]*)/([a-z0-9]*)/([0-9\.]*)/[0-9-]{10}/[a-z0-9]*.xsd"_schemaLocation="../../../\1/\2/\3/\2.xsd"_g
+#s_namespace="http://www.isotc211.org/namespace/([a-z0-9]*)/([0-9\.]*)/[0-9-]{10}"_namespace="http://www.isotc211.org/namespace/\1/\2"_g
+#s_targetNamespace="http://www.isotc211.org/namespace/([a-z0-9]*)/([0-9\.]*)/[0-9-]{10}"_targetNamespace="http://www.isotc211.org/namespace/\1/\2"_
+#
+# I think all we need is
+s_/2014-12-25__g
+s_../../../../1_../../../1_g
+s_../../../../../1_../../../../1_g
+s_../../../../standards.iso.org_../../../standards.iso.org_g
+s_../../../../../standards.iso.org_../../../../standards.iso.org_g
